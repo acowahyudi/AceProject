1248292bfe4e7d56f7f9f7651d5d0fd84dfa0b53
Aco Wahyudi <acoxs0707@gmail.com>
2018-11-09 22:34:09 +0800

    commit tambah button circular

--- AppACESoil/App.js
+++ AppACESoil/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import ActionButton from 'react-native-circular-action-menu';
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>SMART ACE SOIL</Text>
+        <ActionButton style={styles.tombol} buttonColor="#ef7125">
+
+          <ActionButton buttonColor="#6dcdcc" title="new mask" onPress={()=>console.log("note tapped")}/>
+          <ActionButton buttonColor="#6dcdcc" title="new mask" onPress={()=>console.log("note tapped")}/>
+          <ActionButton buttonColor="#6dcdcc" title="new mask" onPress={()=>console.log("note tapped")}/>
+
+        </ActionButton>
       </View>
     );
   }
@@ -14,8 +21,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#114656',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tombol:{
+    margin:2,
+  }
 });
