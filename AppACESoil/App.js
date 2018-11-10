85710489e103eece88f298cdbea308faa999b82d
Aco Wahyudi <acoxs0707@gmail.com>
2018-11-10 23:41:49 +0700

    comit sampah 2

--- AppACESoil/App.js
+++ AppACESoil/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, Text, View, Image, Alert } from 'react-native';
 import ActionButton from 'react-native-circular-action-menu';
+import { Asset, AppLoading, SplashScreen } from 'expo';
 
 
 export default class App extends React.Component {
+  state = {
+    isSplashReady: false,
+    isAppReady: false,
+  };
   render() {
+
     return (
       <View style={styles.container}>
       <Image
               style={{width: 300, height: 150}}
-              source={require('./assets/logo1.png')} />
+              source={require('./assets/logo2.gif')} />
         <Text style={styles.teks}>Smart ACE (Adjusment, Controlling, Evaluation) Soil</Text>
         <ActionButton buttonColor="#fae45a">
 
           <ActionButton buttonColor='#90dc9e' title="New Task" onPress={() => Alert.alert('pH Tanah Aman Terkendali!')}>
             <Image
-              style={{width: 40, height: 90}}
+              style={{width: 50, height: 90}}
               source={require('./assets/ph-meter2.png')} />
           </ActionButton>
 
